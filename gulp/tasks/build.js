1634126e114518735f8dc3f201620e850534ef53
belgac <hans.scheuren@bizliner.com>
2015-11-23 17:01:42 +0000

    chore(gulp): add font managment
    add the gulp task to manage fonts
    
    add the gulp task to copy fonts, create bower overrides to get fonts, create the delete task to
    clean up the fonts folder

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('build:dev',function(callback){
-  runSequence('delete','concat','inject',callback)
+  runSequence('delete',['concat','copy'],'inject',callback)
 });
 
 gulp.task('build:dist',function(callback){
-  runSequence('delete',['minify','concat:css'],'inject',callback) 
+  runSequence('delete',['minify','concat:css','copy'],'inject',callback) 
 });
