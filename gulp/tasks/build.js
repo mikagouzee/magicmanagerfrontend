2919cdbfeb02ec61019052993f9e258566eec94c
belgac <hans.scheuren@bizliner.com>
2015-11-23 10:07:59 +0000

    chore(gulp): add dev build task
    add a task for the development build with run sequence

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('build:dev',function(callback){
-  
+  runSequence('delete','concat',callback)
 });
 
 gulp.task('build:dist',function(callback){
