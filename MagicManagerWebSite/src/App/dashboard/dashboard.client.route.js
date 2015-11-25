9877508186d7cd715bb13264497b897c23baf097
belgac <hans.scheuren@bizliner.com>
2015-11-25 11:40:29 +0000

    feat(app.dashboard): setup basic dashboard views and states
    setup the basic views and states for the dashboard

--- MagicManagerWebSite/src/App/dashboard/dashboard.client.route.js
+++ MagicManagerWebSite/src/App/dashboard/dashboard.client.route.js
@@ -1,25 +1,58 @@
-﻿angular.module(
-    "magicManagerApp.dashboard.routes",
-    ["ui.router"]
-    ).config(dashboardRoute);
+angular.module(
+  'magicManagerApp.dashboard.routes',
+  [
+    'magicManagerApp.templates',
+    'ui.router'
+  ]
+)
+  .config(dashboardRoute);
 
-//todo : add templates when location find
 function dashboardRoute($stateProvider) {
-    $stateProvider.state(
-        "dashboard",
-        { url: "/dashboard", templateUrl: "App/dashboard/dashboard.view.html" }
-        ).state(
-        "dashboard.overview",
-        {url: "/overview", templateUrl: "App/dashboard/overview/overview.view.html" }
-        ).state(
-        "dashboard.market",
-        { url: "/market", templateUrl: "App/dashboard/market/market.view.html" }
-        ).state(
-        "dashboard.stock",
-        { url: "/stock", templateUrl: "App/dashboard/stock/stock.view.html" }
-        ).state(
-        "dashboard.export",
-        { url: "/export", templateUrl: "App/dashboard/export/export.view.html" }
-        )
+    $stateProvider
+      .state(
+        'dashboard',
+        {
+          url: '/dashboard', 
+          templateProvider: function($templateCache){
+            return $templateCache.get('dashboard/dashboard.client.view.html');
+          }
+        }
+      )
+      .state(
+        'dashboard.overview',
+        {
+          url: '/overview', 
+          templateProvider: function($templateCache){
+            return $templateCache.get('dashboard/overview/overview.client.view.html');
+          }
+        }
+      )
+      .state(
+        'dashboard.market',
+        {
+          url: '/market', 
+          templateProvider: function($templateCache){
+            return $templateCache.get('dashboard/market/market.client.view.html');
+         }
+        }
+      )
+      .state(
+        'dashboard.stock',
+        { 
+          url: '/stock', 
+          templateProvider: function($templateCache){
+            return $templateCache.get('dashboard/stock/stock.client.view.html');
+          }
+        }
+      )
+      .state(
+        'dashboard.export',
+        { 
+          url: '/export', 
+          templateProvider: function($templateCache){
+            return $templateCache.get('dashboard/export/export.client.view.html');
+          }
+        }
+      )
 
 }
