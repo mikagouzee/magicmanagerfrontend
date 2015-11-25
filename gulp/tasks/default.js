f5b5af93894b25b809280721365a2d827746ef96
belgac <hans.scheuren@bizliner.com>
2015-11-25 08:31:50 +0000

    chore(gulp.default): add build function to default with prompt
    add build tasks to the default function with a prompt asking if the build should be run for
    development or production

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,6 +1,19 @@
 //it's a gulp task we need gulp
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var inquirer = require('inquirer');
 
-gulp.task('default',function(){
-  return true;
+var questions = [
+  {
+    type: "list",
+    name: "environment",
+    message: "What type of build do you want to run",
+    choices: [ "dev", "dist"]
+  }
+]
+gulp.task('default',function(callback){
+  inquirer.prompt( questions, function( answers ) {
+     runSequence('build:'+answers.environment,callback)
+  })
+ 
 })
