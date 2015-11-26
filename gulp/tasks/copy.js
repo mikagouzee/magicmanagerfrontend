9186c304444ed9c6b03e60bf57cc0a9994620cfd
belgac <hans.scheuren@bizliner.com>
2015-11-26 09:25:36 +0000

    chore(gulp.copy): add image copy
    add a gulp task to copy images

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,11 +4,17 @@ var bowerFiles = require('main-bower-files');
 var gulpFilter = require('gulp-filter');
 
 gulp.task('copy',function(callback){
-  runSequence('copy:fonts',callback)
-})
+  runSequence(['copy:fonts','copy:images'],callback)
+});
+
 gulp.task('copy:fonts',function(){
 	var fontsFilter = gulpFilter(['**/*.eot','**/*.svg','**/*.ttf','**/*.woff','**/*.woff2','**/*.otf']);
   return gulp.src(bowerFiles())
 	.pipe(fontsFilter)
   .pipe(gulp.dest('./MagicManagerWebSite/fonts'))
+});
+
+gulp.task('copy:images',function(){
+  return gulp.src('./MagicManagerWebSite/src/Content/**/*.+(jpg|png|gif)')
+    .pipe(gulp.dest('./MagicManagerWebSite/Content'));
 })
