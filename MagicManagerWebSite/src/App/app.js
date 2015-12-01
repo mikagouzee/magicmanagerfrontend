66bad9fbca91f795cccc4a4ff6b7e42ed6076249
belgac <hans.scheuren@bizliner.com>
2015-12-01 09:15:26 +0000

    docs(app): comment basic app and route

--- MagicManagerWebSite/src/App/app.js
+++ MagicManagerWebSite/src/App/app.js
@@ -1,14 +1,19 @@
 (function () {
     'use strict';
-
+    //create the base App module
     angular.module('magicManagerApp', [
         // Angular modules
 
         // Custom modules
+        // Call the default routes
         "magicManagerApp.routes",
+        //Call the home page module
         "magicManagerApp.home",
+        //Call the dashboard module
         "magicManagerApp.dashboard",
+        //Call the card detail module
         "magicManagerApp.cardDetail",
+        //call the gulp generated $templateCache module
         'magicManagerApp.templates'
 
         // 3rd Party Modules
--- MagicManagerWebSite/src/App/route.client.config.js
+++ MagicManagerWebSite/src/App/route.client.config.js
@@ -6,7 +6,9 @@ angular.module(
 )
   .config(
     function($urlRouterProvider,$locationProvider){
+      //define the default route if no route is given
       $urlRouterProvider.otherwise("/home");
+      //add a ! to the url hashprefix for search engine optimization
       $locationProvider.hashPrefix("!");
     }
 )
