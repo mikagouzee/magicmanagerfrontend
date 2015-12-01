e6a041f1e0b4c4443a94875e30b7ad6151776def
belgac <hans.scheuren@bizliner.com>
2015-12-01 07:35:03 +0000

    fix(csproj): correct the csproj task
    the csproj task wasn't configured properly it added directories instead of adding only files

--- gulp/tasks/csproj.js
+++ gulp/tasks/csproj.js
@@ -4,9 +4,9 @@ var inject = require('gulp-inject');
 gulp.task('csproj',function(){
   return gulp.src('MagicManagerWebSite/MagicManagerWebSite.csproj')
     .pipe(inject(
-      gulp.src(['MagicManagerWebSite/App/**/*','MagicManagerWebSite/Scripts/**/*','MagicManagerWebSite/fonts/**/*','MagicManagerWebSite/Content/**/*','MagicManagerWebSite/src/**/*']),
+      gulp.src(['MagicManagerWebSite/App/**/*','MagicManagerWebSite/Scripts/**/*','MagicManagerWebSite/fonts/**/*','MagicManagerWebSite/Content/**/*','MagicManagerWebSite/src/**/*','MagicManagerWebSite/mockApi/**/*']),
       {
-        starttag: '<!-- gulpinject -->',
+        starttag: '<!-- gulpinject:{{ext}} -->',
         endtag: '<!-- endgulpinject -->',
      		transform: function (filepath) {
           filepath = filepath.replace("/MagicManagerWebSite/", "").replace(/\//g,'\\');  
