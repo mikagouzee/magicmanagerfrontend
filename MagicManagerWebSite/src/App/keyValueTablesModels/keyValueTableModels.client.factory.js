2516873aff65ed781a1e5762d2b80b52f915c11a
belgac <hans.scheuren@bizliner.com>
2015-12-02 07:27:55 +0000

    docs(app.KeyValueTableModel): commented the key value table model getter factory

--- MagicManagerWebSite/src/App/keyValueTablesModels/keyValueTableModels.client.factory.js
+++ MagicManagerWebSite/src/App/keyValueTablesModels/keyValueTableModels.client.factory.js
@@ -1,3 +1,4 @@
+//the factory module to get the fields needed for generating key value tables
 angular.module('magicManagerApp.keyValueTableModels.factory',[])
   .factory(
     'keyValueTableModelsFactory',
@@ -5,29 +6,39 @@ angular.module('magicManagerApp.keyValueTableModels.factory',[])
   )
 
 function keyValueTableModelsFactory ($http, $q) {
+  //prepare the factory variable
   var factory = {};
   
+  //add the function to get key value tables models
   factory.getModel= getModel;
   
+  //return the factory
   return factory;
   
+  // the function to get key value tables models
   function getModel(askedModel){
+    //prepare the promise
     var deferred = $q.defer();
+    //prepare the returned object
     var model = {};
-    
-    //todo : insert api call
+
+    //make the api call to get the models and send the result to the successCallback
     $http.get('/mockApi/keyvaluetablemodels/'+askedModel+'.json').then(successCallback, errorCallback);
     
     function successCallback(response) {
+      //assign the api response data to the returned object
       model = response.data;
+      //resolve the promise
       deferred.resolve(model);
     };
     
+    //if an error occured reject the promise
     function errorCallback(response) {
       model = false;
       deferred.reject(model);
     };
     
+    //return the promise    
     return deferred.promise;
   }
 }
